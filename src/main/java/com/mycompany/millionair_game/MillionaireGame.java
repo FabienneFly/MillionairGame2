4267d6d2ca907a1f9157942195ec40e6af8b3ee9
FabienneFly <fehrfab2@students.zhaw.ch>
2023-06-05 09:14:35 +1200

    small changes

--- src/main/java/com/mycompany/millionair_game/MillionaireGame.java
+++ src/main/java/com/mycompany/millionair_game/MillionaireGame.java
@@ -10,16 +10,14 @@ public class MillionaireGame {
         //New instance of GameStart
 
         new GUI().setVisible(true);
-<<<<<<< Updated upstream
-=======
+
         
-        
+  
           DataBase db = new DataBase();
         db.connectBookStoreDB();
         db.showTable();
         db.closeConnection();
         
->>>>>>> Stashed changes
     }
 
 }
