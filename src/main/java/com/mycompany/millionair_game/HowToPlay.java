8e01b20bf1c8c80b194669446e9cdf23ea3c24af
FabienneFly <fehrfab2@students.zhaw.ch>
2023-06-05 13:04:11 +1200

    HelpScreen

--- src/main/java/com/mycompany/millionair_game/HowToPlay.java
+++ src/main/java/com/mycompany/millionair_game/HowToPlay.java
@@ -29,8 +29,9 @@ public class HowToPlay extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setSize(new java.awt.Dimension(1020, 700));
@@ -41,8 +42,6 @@ public class HowToPlay extends javax.swing.JFrame {
         });
 
         jLabel1.setText("HOW TO PLAY");
-
-        jTextField1.setText("jTextField1");
 
         jButton1.setText("START");
         jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -56,22 +55,27 @@ public class HowToPlay extends javax.swing.JFrame {
             }
         });
 
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jTextArea2.setText("The game consists of a series of multiple-choice questions.\n------------------------------------------------------------------------------------------------------------------------------\n1. Your goal is to answer all the questions correctly and win the top prize of one million dollars.\n2. You will be given four possible answers for each question, and you must choose the one that you think is correct.\n3. You have two \"lifelines\" that you can use to help you answer a question: one 50/50-joker and one Skip-joker.\n4. If you answer a question correctly, you will advance to the next question and your potential winnings will increase.\n5. If you answer a question incorrectly, you will lose all your winnings and the game will be over.\n6. If you reach the end of the game and answer the final question correctly, you will win the top prize.\n------------------------------------------------------------------------------------------------------------------------------\n\nThat's it! Have fun!\n\n------------------------------------------------------------------------------------------------------------------------------\n");
+        jScrollPane2.setViewportView(jTextArea2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(128, Short.MAX_VALUE)
+                .addContainerGap(28, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addGap(450, 450, 450))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 791, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(101, 101, 101))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(404, 404, 404))))
+                        .addGap(404, 404, 404))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 960, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(32, 32, 32))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -79,8 +83,8 @@ public class HowToPlay extends javax.swing.JFrame {
                 .addGap(32, 32, 32)
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 427, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 406, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(115, Short.MAX_VALUE))
         );
@@ -143,7 +147,8 @@ public class HowToPlay extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea2;
     // End of variables declaration//GEN-END:variables
 
 
