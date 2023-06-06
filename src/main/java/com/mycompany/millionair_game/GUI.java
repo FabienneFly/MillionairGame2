1028617a0cef430e32e8168002d107239330e145
Anarchry <83636074+Anarchry@users.noreply.github.com>
2023-06-07 11:35:29 +1200

    GUI class

--- src/main/java/com/mycompany/millionair_game/GUI.java
+++ src/main/java/com/mycompany/millionair_game/GUI.java
@@ -592,6 +592,7 @@ public class GUI extends javax.swing.JFrame
 //        });
 //    }
 
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
